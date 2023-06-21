473b5b1baa27dc3c2f17cc971418138880f6a09f
okharyton <olegkhacv@gmail.com>
2023-06-21 23:29:09 +0300

    feat: change task color when its done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const todoDiv = document.querySelector('.todo-list');
 const addTaskBtn = document.querySelector('.add-task');
 const taskInput = document.querySelector('.task-input');
+const checkbox = document.querySelector('#checkbox');
+const taskLabel = document.querySelector('.task-label');
+
+console.log(checkbox)
 
 let addTask = function (e) {
     e.preventDefault();
@@ -9,8 +13,8 @@ let addTask = function (e) {
     } else {
         todoDiv.insertAdjacentHTML(`beforeend`, `
         <li class="task">
-            <input type="checkbox" id="scales" name="task-name">
-            <label for="task-name">
+            <input type="checkbox" id="checkbox" name="task-name">
+            <label class="task-label" for="task-name">
                     ${taskInput.value}
                 </label>
         <button class="delete w-5\t">
@@ -28,10 +32,27 @@ let addTask = function (e) {
     }
 }
 
+let greenStatus = function () {
+    const listItem = this.closest('li');
+    const taskLabel = listItem.querySelector('.task-label');
+
+    if (this.checked) {
+        taskLabel.style.backgroundColor = 'green';
+    } else {
+        taskLabel.style.backgroundColor = '';
+    }
+}
+
 addTaskBtn.addEventListener('click', addTask);
 taskInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         addTask(e);
     }
-})
+});
+
+document.addEventListener('change', function (e) {
+    if (e.target.matches('.task input[type="checkbox"]')) {
+        greenStatus.call(e.target);
+    }
+});
 
