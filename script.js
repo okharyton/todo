8f807e12eee0f915c129fc451f956a85dac8e504
okharyton <olegkhacv@gmail.com>
2023-07-02 22:28:15 +0300

    refactor: change code from functional to OOP

--- script.js
+++ script.js
@@ -1,130 +1,138 @@
-const todoDiv = document.querySelector('.todo-list');
-const addTaskBtn = document.querySelector('.add-task');
-const taskInput = document.querySelector('.task-input');
-const checkbox = document.querySelector('#checkbox');
-const taskLabel = document.querySelector('.task-label');
-let editButtons = document.querySelectorAll('.edit');
-const showCompletedTasksBtn = document.querySelector('.show-completed-tasks');
-const showAllTasksBtn = document.querySelector('.show-all-tasks');
+class TodoList {
+    constructor() {
+        this.todoDiv = document.querySelector('.todo-list');
+        this.addTaskBtn = document.querySelector('.add-task');
+        this.taskInput = document.querySelector('.task-input');
+        this.showCompletedTasksBtn = document.querySelector('.show-completed-tasks');
+        this.showAllTasksBtn = document.querySelector('.show-all-tasks');
+        this.tasks = [];
+        this.loadTasksFromLocalStorage();
+        this.addEventListeners();
+        this.renderTasks();
+    }
 
-let addTask = function (e) {
-    e.preventDefault();
-    if (taskInput.value.length === 0) {
-        alert('Enter Task Name');
-    } else {
-        todoDiv.insertAdjacentHTML(`beforeend`, `
-    <div class="task">
-        <ul class="task__container">
-            <li class="task__line">
-                <input type="checkbox" id="checkbox" name="task-name">
-                <label class="task-label" for="task-name">
-                    ${taskInput.value}
-                </label>
-            </li>
-            <div class="buttons">
-                <button class="edit">
-                    <i class="fas fa-edit" style="color: #121212;"></i>
-                </button>
-                <button class="delete w-5">
-                    <i class="fa fa-trash" aria-hidden="true"></i>
-                </button>
-            </div>
-        </ul>
-    </div>
-`);
-        let editButtons = document.querySelectorAll('.edit');
-        editButtons.forEach(function (editButton) {
-            editButton.addEventListener('click', editTask);
+    addEventListeners() {
+        this.addTaskBtn.addEventListener('click', this.addTask.bind(this));
+        this.taskInput.addEventListener('keypress', (e) => {
+            if (e.key === 'Enter') {
+                this.addTask(e);
+            }
+        });
+        this.showCompletedTasksBtn.addEventListener('click', this.showCompletedTasks.bind(this));
+        this.showAllTasksBtn.addEventListener('click', this.showAllTasks.bind(this));
+    }
+
+    addTask(e) {
+        e.preventDefault();
+        if (this.taskInput.value.length === 0) {
+            alert('Enter Task Name');
+        } else {
+            const taskName = this.taskInput.value;
+            const task = {
+                id: Date.now(),
+                name: taskName,
+                completed: false
+            };
+            this.tasks.push(task);
+            this.saveTasksToLocalStorage();
+            this.renderTask(task);
+            this.taskInput.value = '';
+        }
+    }
+
+    renderTask(task) {
+        const taskItem = document.createElement('div');
+        taskItem.className = 'task';
+        taskItem.innerHTML = `
+      <div class="task__container">
+        <div class="task-line">
+          <input type="checkbox" name="task-name" ${task.completed ? 'checked' : ''}>
+          <label class="task-label" data-task-id="${task.id}">${task.name}</label>
+        </div>
+        <div class="buttons">
+          <button class="edit">
+            <i class="fas fa-edit" style="color: #121212;"></i>
+          </button>
+          <button class="delete w-5">
+            <i class="fa fa-trash" aria-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+    `;
+
+        const deleteButton = taskItem.querySelector('.delete');
+        deleteButton.addEventListener('click', () => {
+            this.deleteTask(task.id);
         });
 
-        let currentTasks = document.querySelectorAll('.delete');
-        for (let i = 0; i <currentTasks.length; i++) {
-            currentTasks[i].addEventListener('click', function () {
-                const taskContainer = this.closest('.task');
-                taskContainer.style.backgroundColor = '';
-                taskContainer.remove();
-            })
+        const checkbox = taskItem.querySelector('input[type="checkbox"]');
+        checkbox.addEventListener('change', () => {
+            this.toggleTaskCompleted(task.id);
+        });
+
+        const editButton = taskItem.querySelector('.edit');
+        editButton.addEventListener('click', () => {
+            this.editTask(task.id);
+        });
+
+        this.todoDiv.appendChild(taskItem);
+    }
+
+    deleteTask(taskId) {
+        this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        this.saveTasksToLocalStorage();
+        this.renderTasks();
+    }
+
+    toggleTaskCompleted(taskId) {
+        const task = this.tasks.find((task) => task.id === taskId);
+        if (task) {
+            task.completed = !task.completed;
+            this.saveTasksToLocalStorage();
         }
-        document.querySelector('.task-input').value = '';
+    }
+
+    editTask(taskId) {
+        const task = this.tasks.find((task) => task.id === taskId);
+        if (task) {
+            const taskLabel = document.querySelector(`[data-task-id="${taskId}"]`);
+
+taskLabel.contentEditable = true;
+taskLabel.focus();
+            taskLabel.addEventListener('keydown', (e) => {
+                if (e.key === 'Enter') {
+                    e.preventDefault();
+                    taskLabel.blur();
+                }
+            });
+
+            taskLabel.addEventListener('blur', () => {
+                taskLabel.contentEditable = false;
+                task.name = taskLabel.textContent;
+                this.saveTasksToLocalStorage();
+            });
+        }}
+    showCompletedTasks() {
+        const completedTasks = this.tasks.filter((task) => task.completed);
+        this.renderTasks(completedTasks);
+    }
+    showAllTasks() {
+        this.renderTasks();
+    }
+    saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+    loadTasksFromLocalStorage() {
+        const storedTasks = localStorage.getItem('tasks');
+        if (storedTasks) {
+            this.tasks = JSON.parse(storedTasks);
+        }
+    }
+    renderTasks(tasks = this.tasks) {
+        this.todoDiv.innerHTML = '';
+        tasks.forEach((task) => {
+            this.renderTask(task);
+        });
     }
 }
-
-let greenStatus = function () {
-    const taskContainer = this.closest('.task');
-
-    if (this.checked) {
-        taskContainer.style.backgroundColor = '#2BAE66';
-    } else {
-        taskContainer.style.backgroundColor = '';
-    }
-}
-
-addTaskBtn.addEventListener('click', addTask);
-taskInput.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        addTask(e);
-    }
-});
-
-document.addEventListener('change', function (e) {
-    if (e.target.matches('.task input[type="checkbox"]')) {
-        greenStatus.call(e.target);
-    }
-});
-
-let editTask = function () {
-    const taskContainer = this.closest('.task');
-    const taskLabel = taskContainer.querySelector('.task-label');
-
-    taskLabel.setAttribute('contenteditable', 'true');
-    taskLabel.focus();
-
-    const selection = window.getSelection();
-    const range = document.createRange();
-    range.selectNodeContents(taskLabel);
-    range.collapse(false);
-    selection.removeAllRanges();
-    selection.addRange(range);
-
-    taskLabel.addEventListener('keydown', function (e) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            taskLabel.blur();
-        }
-    });
-    taskLabel.addEventListener('blur', function () {
-        taskLabel.removeAttribute('contenteditable');
-    });
-};
-
-let saveTask = function () {
-    const taskContainer = this.closest('.task');
-    const taskLabel = taskContainer.querySelector('.task-label');
-
-    taskLabel.removeAttribute('contenteditable');
-}
-
-let showCompletedTasks = function () {
-    const tasks = document.querySelectorAll('.task');
-    tasks.forEach(function (task) {
-        const checkbox = task.querySelector('input[type="checkbox"]');
-        if (checkbox.checked) {
-            task.style.display = 'block';
-        } else {
-            task.style.display = 'none';
-        }
-    });
-};
-
-let showAllTasks = function () {
-    const tasks = document.querySelectorAll('.task');
-    tasks.forEach(function (task) {
-        task.style.display = 'block';
-    });
-};
-
-editButtons.forEach(function (editButton) {
-    editButton.addEventListener('click', editTask);
-});
-showCompletedTasksBtn.addEventListener('click', showCompletedTasks);
-showAllTasksBtn.addEventListener('click', showAllTasks);
+const todoList = new TodoList();
